606963541551177fedeaa8689aff4dda1bf7f3a5
Amit Majumdar <amit.majumdar89@gmail.com>
2018-12-26 18:10:22 +0530

    changes

--- UtilityFramework/ExampleProject/ViewController.swift
+++ UtilityFramework/ExampleProject/ViewController.swift
@@ -13,27 +13,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let date = Date.current
-        print(date.day)
-        print(date.month)
-        print(date.year)
-        print(date.yearForWeekOfYear)
-        print(date.weekOfYear)
-        print(date.weekOfMonth)
-        print(date.weekdayOrdinal)
-        print(date.weekday)
-        print(date.nanosecond)
-        print(date.timeZone)
-        print(date.second)
-        print(date.hour)
-        print(date.minute)
-        print(date.quarter)
-        print(date.weekdaySymbol)
-        print(date.shortWeekdaySymbol)
-        print(date.monthSymbol)
-        print(date.shortMonthSymbol)
-        print(date.standaloneWeekdaySymbol)
-        print(Date.getCustomDate(day: 1, month: 1, year: 1990).endOfDay)
     }
 }
 
